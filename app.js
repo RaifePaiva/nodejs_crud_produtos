f9420634583ee6336d9d37c80ea97b30e9d500d0
RaifePaiva <@Casa350>
2023-09-01 02:21:20 -0300

    correção de versão node

--- app.js
+++ app.js
@@ -112,5 +112,5 @@ app.get('/test', (req, res) => {
 
 //Server
 app.listen(process.env.PORT || 3000, () => {
-  console.log('Servidor iniciado na porta 3000');
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
